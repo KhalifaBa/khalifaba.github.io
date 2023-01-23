8553810c3fde0475c0f82a6c6da998e28ff3d94f
Khalifa <khalifalo178@gmail.com>
2023-01-23 12:18:47 +0100

    Modified star.style.top to left

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let header = document.querySelector('header')
 window.addEventListener('scroll',function ()
 {
     let value = window.scrollY;
-    stars.style.top = value * 0.25 + 'px';
-    introduce.style.marginTop = value * 1.05 + 'px';
+    stars.style.left = value * 0.25 + 'px';
+    introduce.style.marginTop = value * 1.5 + 'px';
     mountain.style.top = value * 0.5 + 'px';
     header.style.top = value * 0.5 + 'px';
 
